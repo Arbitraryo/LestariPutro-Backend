bfeb3417dff745c8d9fc5a28495e6557f3d21d92
Arbitraryo <ilhamrizqipratama@gmail.com>
2025-07-31 19:27:19 +0700

    Fixing uploading picture admin 2

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -7,19 +7,7 @@ use Illuminate\Http\Middleware\TrustProxies as Middleware;
 
 class TrustProxies extends Middleware
 {
-    /**
-     * The trusted proxies for this application.
-     *
-     * Use '*' to trust all proxies, or define IPs/hosts.
-     *
-     * @var array|string|null
-     */
     protected $proxies = '*';
 
-    /**
-     * The headers that should be used to detect proxies.
-     *
-     * @var int
-     */
     protected $headers = Request::HEADER_X_FORWARDED_ALL;
 }
