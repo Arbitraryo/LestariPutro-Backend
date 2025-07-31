eb7f6ae117f48ae73e8552330866c7fcd9426f53
Arbitraryo <ilhamrizqipratama@gmail.com>
2025-07-31 16:14:19 +0700

    Fixing uploading picture admin 2

---
+++ app/Http/Kernel.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Http;
+
+use Illuminate\Foundation\Http\Kernel as HttpKernel;
+
+class Kernel extends HttpKernel
+{
+    /**
+     * Global HTTP middleware stack.
+     */
+    protected $middleware = [
+        // Handles proxy headers (like X-Forwarded-For)
+        \App\Http\Middleware\TrustProxies::class,
+
+        // Prevents requests during maintenance
+        \App\Http\Middleware\PreventRequestsDuringMaintenance::class,
+
+        // Validates POST size
+        \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
+
+        // Trims whitespace from request input
+        \App\Http\Middleware\TrimStrings::class,
+
+        // Converts empty strings to null
+        \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
+    ];
+
+    /**
+     * Route middleware groups.
+     */
+    protected $middlewareGroups = [
+        'web' => [
+            \App\Http\Middleware\EncryptCookies::class,
+            \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
+            \Illuminate\Session\Middleware\StartSession::class,
+            \Illuminate\View\Middleware\ShareErrorsFromSession::class,
+            \App\Http\Middleware\VerifyCsrfToken::class,
+            \Illuminate\Routing\Middleware\SubstituteBindings::class,
+        ],
+
+        'api' => [
+            \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
+            \Illuminate\Routing\Middleware\ThrottleRequests::class.':api',
+            \Illuminate\Routing\Middleware\SubstituteBindings::class,
+        ],
+    ];
+
+    /**
+     * Route middleware.
+     */
+    protected $routeMiddleware = [
+        'auth' => \App\Http\Middleware\Authenticate::class,
+        'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
+        'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
+        'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+    ];
+}
